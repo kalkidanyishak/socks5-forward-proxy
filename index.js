1ea96306ecb2b004eccc05b16736ee99c28847d4
kalkidanyishak <94622211+kalkidanyishak@users.noreply.github.com>
2023-07-28 16:23:40 +0300

    Update index.js changed the port value to resemble the environment port or fall back to 3001

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-var socks = require('socksv5');
+const socks = require('socksv5');
+const port = process.env.PORT || 3001;
 
-var srv = socks.createServer(function(info, accept, deny) {
+const srv = socks.createServer(function(info, accept, deny) {
   accept();
 });
-srv.listen(1080, 'localhost', function() {
-  console.log('SOCKS server listening on port 1080');
+srv.listen(port, 'localhost', function() {
+  console.log('SOCKS server listening on port '+port);
 });
 
 srv.useAuth(socks.auth.UserPassword(function(user, password, cb) {
